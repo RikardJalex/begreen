27ee7529b9ac78807b3ca0d067a8d718493e002d
Johan Mackegard <johan.mackegard@gmail.com>
2019-01-07 11:37:25 +0100

    first commit

---
+++ src/main/java/com/begreen/BegreenApplication.java
@@ -0,0 +1,14 @@
+package com.begreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BegreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BegreenApplication.class, args);
+	}
+
+}
+
