d5b1a64b2ae5946297cc07a8b6c13f103a602d98
Rikard Jalex <*rikardjalex@gmail.com*>
2019-01-18 10:20:19 +0100

    för att störa Hamid

--- src/main/java/com/begreen/DatabaseCon.java
+++ src/main/java/com/begreen/DatabaseCon.java
@@ -14,7 +14,7 @@ public class DatabaseCon {
         System.out.println("Creating connection..");
         try {
             System.out.println("uppkopplad");
-            return DriverManager.getConnection(url, "root", "Offer123");
+            return DriverManager.getConnection(url, "root", "password");
         } catch (SQLException e) {
             e.printStackTrace();
             return null;
